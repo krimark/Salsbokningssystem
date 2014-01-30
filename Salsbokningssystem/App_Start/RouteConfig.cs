b0d44d07764bc3263a659b9777976b69ba6b0916
Bettsson <petersson.elisabeth@gmail.com>
2014-01-30 15:49:18 +0100

    La in nuGet för schemafunktion
    Ligger bara en exempelfil än så länge...

--- Salsbokningssystem/App_Start/RouteConfig.cs
+++ Salsbokningssystem/App_Start/RouteConfig.cs
@@ -18,7 +18,6 @@ namespace Salsbokningssystem
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
             );
-
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
