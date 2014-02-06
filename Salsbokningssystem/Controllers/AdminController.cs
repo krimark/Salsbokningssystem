a89a5086bcb0d26babdbb037e275520ad4c55f96
Bettsson <petersson.elisabeth@gmail.com>
2014-02-06 15:20:08 +0100

    Booking history med sökfunktion inlagt för admin. Syns ej än!

--- Salsbokningssystem/Controllers/AdminController.cs
+++ Salsbokningssystem/Controllers/AdminController.cs
@@ -1,4 +1,10 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Salsbokningssystem.Models;
+
 
 namespace Salsbokningssystem.Controllers
 {
@@ -7,12 +13,33 @@ namespace Salsbokningssystem.Controllers
     {
         //
         // GET: /Admin/
+        DataClasses1DataContext db = new DataClasses1DataContext();
 
         public ActionResult Index()
         {
             ViewBag.Message = "Här kan Gun administrera";
 
             return View();
+        }
+
+        public ActionResult BookingHistory(string searchString)
+        {
+
+            var bookings = from b in db.Bookings
+                           orderby b.StartTime
+                           select b;
+
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                bookings = from r in db.Bookings
+                           where r.Room.Name.Contains(searchString) || r.User.UserName.Contains(searchString)
+                           orderby r.StartTime
+                           select r;
+            }
+
+            return View(bookings);
+
         }
 
     }
