1416f701b6657c8f26fc9e7c535accb53e243dee
Kristofer <kristofer.hindersson@gmail.com>
2014-02-13 09:33:24 +0100

    Snyggade till lite

--- Salsbokningssystem/Controllers/BookingController.cs
+++ Salsbokningssystem/Controllers/BookingController.cs
@@ -29,8 +29,6 @@ namespace Salsbokningssystem.Controllers
             return View();
         }
 
-
-
         [HttpPost]
         public ActionResult Book(Booking booking, FormCollection form)
         {
@@ -46,7 +44,7 @@ namespace Salsbokningssystem.Controllers
                     ViewBag.Error = "Fel bookning START TIME måste vara tidgare än END TIME";
                     return View();
                 }
-                if (PreviousBookingExists(booking.StartTime, booking.EndTime, booking.RoomID))
+                if (PreviousBookingExists(booking.RoomID, booking.StartTime, booking.EndTime))
                 {
                     TempData["UserMessage"] = "Bokningstiden du angav krockar med en existerande bokning.";
                     return RedirectToAction("Index");
@@ -99,7 +97,7 @@ namespace Salsbokningssystem.Controllers
                     TempData["UserMessage"] = "Du har redan gjort en bokning!";
                     return RedirectToAction("Index");
                 }
-                if (PreviousBookingExists(booking.StartTime, booking.EndTime, booking.RoomID))
+                if (PreviousBookingExists(booking.RoomID, booking.StartTime, booking.EndTime))
                 {
                     TempData["UserMessage"] = "Bokningstiden du angav krockar med en existerande bokning.";
                     return RedirectToAction("Index");
@@ -148,7 +146,7 @@ namespace Salsbokningssystem.Controllers
             return RedirectToAction("Index", "Booking");
         }
 
-        private bool PreviousBookingExists(DateTime bookingStart, DateTime bookingEnd, int roomId)
+        private bool PreviousBookingExists(int roomId, DateTime bookingStart, DateTime bookingEnd)
         {
             var result = (from b in db.Bookings
                           where b.RoomID == roomId && b.StartTime < bookingEnd && bookingStart < b.EndTime
